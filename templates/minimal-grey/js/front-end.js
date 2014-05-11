cc2241efa3272bce92a72ffcd33935b7e7ac3e3b
lexoyo <a.hoyau@silexlabs.org>
2014-05-11 22:55:37 +0200

    fixed minimal grey bg

--- templates/minimal-grey/js/front-end.js
+++ templates/minimal-grey/js/front-end.js
@@ -10,6 +10,10 @@
 //////////////////////////////////////////////////
 
 $(function() {
+  // allow HTML5 tags used by Silex to be styled with CSS (polyfill)
+  document.createElement('HEADER');
+  document.createElement('VIDEO');
+
   /**
    * list all pages from the head section
    * and open the 1st one by default
@@ -49,26 +53,4 @@ $(function() {
       window.open(href, '_blank');
     }
   });
-  /**
-   * resize body to the size of its content
-   * This causes the main container to be left aligned
-   *
-  function onResize(event){
-    var width = 0;
-    var height = 0;
-    $(".background").each(function (index) {
-      var position = $(this).position();
-      var right = position.left + $(this).width();
-      var bottom = position.top + $(this).height();
-      if (width < right) width = right;
-      if (height < bottom) height = bottom;
-    });
-    $("body").css({
-      "min-width": width + "px",
-      "min-height": height + "px"
-    });
-  }
-  // call it at start
-  onResize();
-  /* */
 })
