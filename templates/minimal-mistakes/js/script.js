4620a94e576643f9e625d1d2687ede2eabfc346f
lexoyo <a.hoyau@silexlabs.org>
2014-06-01 21:20:27 +0200

    added minimal-mistakes template

---
+++ templates/minimal-mistakes/js/script.js
@@ -0,0 +1,5 @@
+////////////////////////////////////
+// animate.js
+// https://github.com/silexlabs/Silex/issues/87
+document.write('<link href="./css/animate.min.css" rel="stylesheet">');
+////////////////////////////////////
