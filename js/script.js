2247ad902f036f5e0ca33b93abbef327fbaf64d5
lexoyo <a.hoyau@silexlabs.org>
2014-06-02 22:38:14 +0200

    fixed minimal mistake on small screens added google analytics everywhere

--- js/script.js
+++ js/script.js
@@ -15,10 +15,11 @@ $(function(){
 });
 // **
 // google analytics
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-try {
-  var pageTracker = _gat._getTracker("UA-19608894-20");
-pageTracker._trackPageview();
-} catch(err) {}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-19608894-20', 'silexlabs.github.io');
+ga('send', 'pageview');
 // **
