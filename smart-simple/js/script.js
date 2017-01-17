0340567a2ce20edeb479647f1a479aefdd477859
singchan <billy321@im-paris.fr>
2017-01-17 22:18:40 +0100

    Create smart-simple/js/script.js

--- smart-simple/js/script.js
+++ smart-simple/js/script.js
@@ -33,3 +33,21 @@
         });
     });
     
+    
+    
+    
+    
+/*
+animation widget from here: https://github.com/silexlabs/Silex/issues/443
+*/
+$(function() {
+    window.sr = ScrollReveal({
+        distance: '100px'
+    });
+    sr.reveal('.from-left', { origin:  'left'});
+    sr.reveal('.from-right', { origin:  'right'});
+    sr.reveal('.from-top', { origin:  'top'});
+    sr.reveal('.from-bottom', { origin:  'bottom'});
+
+})
+    
