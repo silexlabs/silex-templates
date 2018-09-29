8de8db9fea47772c26265f47c26cc72c43a79fd4
Alex Hoyau <a.hoyau@silexlabs.org>
2018-09-29 13:31:18 +0200

    Batch update

--- ipsoom/js/script.js
+++ ipsoom/js/script.js
@@ -1,2 +1,15 @@
 
-    
+/* slider buttons */
+$(function() {
+    try {
+        var slider = $('.slider .unslider').children().get(0);
+        var unslider = $(slider).data('unslider');
+        $('.sub-nav').click(function() {
+            unslider.next()
+            $('.sub-nav-active').removeClass('sub-nav-active');
+            $(this).addClass('sub-nav-active');
+        });
+    }
+    catch(e) {
+    }
+})
