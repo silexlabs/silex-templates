3c2c94ec6e13f363804c2a18796f579ce9e08adc
Alex Hoyau <a.hoyau@silexlabs.org>
2018-12-26 15:30:31 +0200

    Batch update

--- food-and-drink/js/script.js
+++ food-and-drink/js/script.js
@@ -1,3 +1,11 @@
 
-        /* */
-    
+$(function() {
+  var $container = $('<div id="smooth-container"></div>');
+  var $body = $('body');
+  
+  $body.children().appendTo($container);
+  
+  $container
+  .appendTo($body)
+  .smoothState();
+});
