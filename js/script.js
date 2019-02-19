11ce19c2c62a964d64d541ed54ce3bb3904df38e
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2019-02-19 23:35:17 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
+            });
+        
+            /*
+                animation widget from here: https://github.com/silexlabs/Silex/issues/443
+            */
+        
+            window.sr = ScrollReveal({
+                distance: '100px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+        
+        })
+    
